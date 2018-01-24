a35b53f9d5f2001c926cb2c2f0e7f878126b435d
Gravechapa <Gravechapa@protonmail.com>
2018-01-25 02:42:29 +0300

    update

--- src/acpi_2.rs
+++ src/acpi_2.rs
@@ -8,18 +8,26 @@ pub struct ACPI2Tag
     rsdp_2: RSDP2,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 #[repr(packed)]
 pub struct RSDP2
 {
-    signature: [u8; 8],
-    checksum: u8,
-    oem_id: [u8; 6],
-    revision: u8,
-    rsdt_address: u32,
+    pub signature: [u8; 8],
+    pub checksum: u8,
+    pub oem_id: [u8; 6],
+    pub revision: u8,
+    pub rsdt_address: u32,
     //v2 part
-    length: u32,
-    xsdt_address: u64,
-    extended_checksum: u8,
-    reserved: [u8; 3],
+    pub length: u32,
+    pub xsdt_address: u64,
+    pub extended_checksum: u8,
+    pub reserved: [u8; 3],
+}
+
+impl ACPI2Tag
+{
+    pub fn get_rsdp(&self) -> RSDP2
+    {
+        self.rsdp_2.clone()
+    }
 }
